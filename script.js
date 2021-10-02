5138950adfcd90cebcf5542f3ab1c4e869e9167c
Nikolai H <nikolai.humphreyb@gmail.com>
2021-10-02 19:53:22 -0400

    add filter function

--- script.js
+++ script.js
@@ -5,6 +5,9 @@
 
 
 // add filtering feature
+// my god after all that hullaballoo I just realized I can just reorder the flexbox when filter function is run
+// I don't have to fully regenerate the page and rerun functions on bookmarks etc
+// oh. my god.
 
 // add skinning options for CSS (remember to use variables)
 
@@ -13,9 +16,10 @@
 const fullLibrary = []
 
 let libraryEntry = class {
-    constructor (title, author, id, summ, year, chap, wc) {
+    constructor (title, authorFirst, authorLast, id, summ, year, chap, wc) {
         this.title = title;
-        this.author = author;
+        this.authorFirst = authorFirst;
+        this.authorLast = authorLast;
         this.id = id;
         this.summ = summ;
         this.year = year;
@@ -25,16 +29,19 @@ let libraryEntry = class {
     }
 }
 
-let id1 = new libraryEntry('Dracula', 'Bram Stoker', 'id1',
+let id1 = new libraryEntry('Dracula', 'Bram', 'Stoker', 'id1',
 'The classic vampire novel.',
 1897, 27, 161774)
-let id2 = new libraryEntry('Moby Dick', 'Herman Melville', 'id2',
+let id2 = new libraryEntry('Moby Dick', 'Herman', 'Melville', 'id2',
 "Call me Ishmael. Some years ago—never mind how long precisely—having little or no money in my purse, and nothing particular to interest me on shore, I thought I would sail about a little and see the watery part of the world.",
 1851, 135, 206052)
-let id3 = new libraryEntry('Pride and Prejudice', 'Jane Austen', 'id3',
+let id3 = new libraryEntry('Pride and Prejudice', 'Jane', 'Austen', 'id3',
 "It is a truth universally acknowledged, that a single man in possession of a good fortune, must be in want of a wife. <br> However little known the feelings or views of such a man may be on his first entering a neighbourhood, this truth is so well fixed in the minds of the surrounding families, that he is considered as the rightful property of some one or other of their daughters.",
  1813, 61, 124713)
-let id4 = new libraryEntry('The Picture of Dorian Gray', 'Oscar Wilde', 'id4', 'A classic philosophical novel', 1891, 20, 65105)
+ let id4 = new libraryEntry('The Strange Case of Dr. Jekyll and Mr. Hyde', 'Robert Louis','Stevenson', 'id4', 'summary goes here', 1886, 10, 25497)
+ let id5 = new libraryEntry('The Picture of Dorian Gray', 'Oscar', 'Wilde', 'id5', 'A classic philosophical novel', 1891, 20, 65105)
+
+
 
 //to use when creating blurbs so URL is correctly formatted
 function getURL (work) {
@@ -43,24 +50,75 @@ function getURL (work) {
     return URL
 }
 
+
+//------INITIALIZE LIBRARY-------//
+
+let library = []
+
+if (sessionStorage.getItem('library')) {
+    library = JSON.parse(sessionStorage.getItem('library'))
+} else {
+    sessionStorage.setItem('library', JSON.stringify(library))
+}
+
+let getLibrary = sessionStorage.getItem('library')
+
+let bookmarks = document.querySelectorAll('.actions span')
+
 //-------BROWSING---------//
 
 if (document.getElementById('browse')) {
     populateBrowse()
 }
 
+if (document.getElementById('sort-by')) {
+    const sortBy = document.getElementById('sort-by')
+    sortBy.addEventListener('change', (e) => {
+        let blurbSpace = document.querySelector('.blurb-space')
+        while (blurbSpace.firstChild) {
+            blurbSpace.removeChild(blurbSpace.firstChild)
+        }
+        let sortedLibrary = filterBooks(fullLibrary) //make sure to change this and maybe add if variable if you add filter to my-library
+        for (let i = 0; i < sortedLibrary.length; i++) {
+            createBlurb(sortedLibrary[i])
+        }
+        enableBookmarks()
+        findBookmarks()
+    })
+}
+
+function filterBooks (array) {
+    const sortBy = document.getElementById('sort-by')
+    let currentSort = sortBy.value
+    let newSort
+    if (currentSort === 'title') {
+        newSort = array.sort((a, b) => {
+            return a.title < b.title ? -1 : 1;
+        })
+    }
+    if (currentSort === 'title-reverse') {
+        newSort = array.sort((a, b) => {
+            return a.title > b.title ? -1 : 1;
+        })
+    }
+    console.log(newSort)
+    return newSort
+}
 
 function populateBrowse () {
-    for (let i = 0; i < fullLibrary.length; i++) {
-        createBlurb(fullLibrary[i])
+    let sortedLibrary = filterBooks(fullLibrary)
+    for (let i = 0; i < sortedLibrary.length; i++) {
+        createBlurb(sortedLibrary[i])
     }
+    enableBookmarks()
+    findBookmarks()
 }
 
 function createBlurb (currentBook) {
     const blurbSpace = document.querySelector('.blurb-space')
         // get properties of current book object
     let currentTitle = currentBook.title
-    let currentAuthor = currentBook.author
+    let currentAuthor = `${currentBook.authorFirst} ${currentBook.authorLast}`
     let currentID = currentBook.id
     let currentSumm = currentBook.summ
     let currentYear = currentBook.year
@@ -103,18 +161,6 @@ function createBlurb (currentBook) {
     blurbSpace.appendChild(newBlurb)
 }
 
-//------INITIALIZE LIBRARY-------//
-
-let library = []
-
-if (sessionStorage.getItem('library')) {
-    library = JSON.parse(sessionStorage.getItem('library'))
-} else {
-    sessionStorage.setItem('library', JSON.stringify(library))
-}
-
-let getLibrary = sessionStorage.getItem('library')
-
 
 //-------PERSONAL LIBRARY ---------//
 
@@ -131,6 +177,8 @@ function populateLibrary () {
         console.log(thisWork)
         createBlurb(thisWork)
     }
+    enableBookmarks()
+    findBookmarks()
 }
 
 //----------BOOK PAGES-----------//
@@ -147,49 +195,64 @@ function populateBook () {
     let thisIndex = fullLibrary.findIndex((item) => item.id === thisID)
     let thisWork = fullLibrary[thisIndex]
     createBlurb(thisWork)
+    enableBookmarks()
+    findBookmarks()
 }
+
+//------------FILTER FUNCTION
+//UNFINISHED------------//
+// if (document.getElementById('filter-box')) {
+//     filterBooks()
+// }
+
+// function filterBooks() {
+//     const sortBy = document.getElementById('sort-by')
+//     const currentSort = sortBy.value
+// }
 
 //------- BOOKMARKING FUNCTIONALITY --------//
 
-const bookmarks = document.querySelectorAll('.actions span')
-//NOTE this must be placed AFTER the blurb creation script
-// otherwise it will not have the newly created blurbs in its node list and bookmark scripts won't function
+//bookmark initialization happens in library management, otherwise blah blah called before initialization etc
+//need to rerun functions after regenerating list, eg filtering
 
-bookmarks.forEach((bookmark) => {
-    let currentWork = bookmark.parentNode.parentNode.id
-    console.log(currentWork)
-    if (getLibrary.includes(currentWork)) {
-        bookmark.className = 'bookmark-fill'
-    }
-})
-
-bookmarks.forEach((bookmark) => {
-    bookmark.addEventListener('click', function (e) {
-        e.preventDefault
-        let currentWork = this.parentNode.parentNode.id
-        if (this.className === 'bookmark') {
-            this.className = 'bookmark-fill'
-            setBookmark(currentWork)
-        } else if (this.className === 'bookmark-fill') {
-            this.className = 'bookmark'
-            removeBookmark(currentWork)
+function enableBookmarks () {
+    bookmarks = document.querySelectorAll('.actions span')
+    
+    bookmarks.forEach((bookmark) => {
+        let currentWork = bookmark.parentNode.parentNode.id
+        let library = JSON.parse(sessionStorage.getItem('library'))
+        console.log(currentWork)
+        if (library.includes(currentWork)) {
+            bookmark.className = 'bookmark-fill'
+        } else {
+            bookmark.className = 'bookmark'
         }
-    //select innerText of h2 of parent element(book title)
-    //CRIMINALLY UGLY:
-    // let currentTitle = this.parentNode.parentNode.firstChild.nextSibling.firstChild.nextSibling.innerText
     })
-})
+}
+
+function findBookmarks () {
+    bookmarks = document.querySelectorAll('.actions span')
+    bookmarks.forEach((bookmark) => {
+        bookmark.addEventListener('click', function (e) {
+            e.preventDefault
+            let currentWork = this.parentNode.parentNode.id
+            if (this.className === 'bookmark') {
+                this.className = 'bookmark-fill'
+                setBookmark(currentWork)
+            } else if (this.className === 'bookmark-fill') {
+                this.className = 'bookmark'
+                removeBookmark(currentWork)
+            }
+        })
+    })
+}
+
 
 function setBookmark (work) {
         console.log('initial load is ' + library)
         library.push(work)
         sessionStorage.setItem('library', JSON.stringify(library))
         console.log('session storage is ' + JSON.parse(sessionStorage.getItem('library')))
-
-    // library.books.push(work)
-    // console.log(library.books)
-
-    // localStorage.setItem()
 }
 
 function removeBookmark (work) {
